92db5fa25c0834ae19a4cc0f3b3b18ed2bb75027
Michael Jones <michael.jones90@me.com>
2026-06-28 20:43:13 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-BucketList/HackingWithSwiftUI_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_BucketListApp.swift
+//  HackingWithSwiftUI-BucketList
+//
+//  Created by Michael Jones on 28/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
