a4974d77aee869d2617433c3a679f69e8b583942
Michael Jones <michael.jones90@me.com>
2026-07-01 13:38:54 +0100

    Added a computed property to store the coordinates as a CLLocationCoordinate2D. An Equatable function added using the ID as the key.

--- HackingWithSwiftUI-BucketList/Model/Location.swift
+++ HackingWithSwiftUI-BucketList/Model/Location.swift
@@ -5,6 +5,7 @@
 //  Created by Michael Jones on 01/07/2026.
 //
 
+import MapKit
 import Foundation
 
 /// A Struct for storing information about geographic locations.
@@ -14,4 +15,19 @@ struct Location: Codable, Equatable, Identifiable {
     let description: String
     var latitude: Double
     var longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    #if DEBUG
+    /// This section will not get compiled into the release build.
+    static let example = Location(id: UUID(), name: "Camberley", description: "", latitude: 51.33705, longitude: -0.74261)
+    #endif
+    
+    
 }
