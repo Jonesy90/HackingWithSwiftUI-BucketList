1326d22e7754d31056a02ac438ac88a75b4977f5
Michael Jones <michael.jones90@me.com>
2026-07-01 16:59:02 +0100

    Updates to the sorting of the nearby places and the text shown when the places have loaded successfully

--- HackingWithSwiftUI-BucketList/Views/EditView.swift
+++ HackingWithSwiftUI-BucketList/Views/EditView.swift
@@ -49,7 +49,7 @@ struct EditView: View {
                         Text("Loading...")
                     case .loaded:
                         ForEach(pages, id: \.pageid) { page in
-                            Text("\(Text(page.title).font(.headline)): \(Text("Page Description here").italic())")
+                            Text("\(Text(page.title).font(.headline)): \(Text(page.description).italic())")
                         }
                     case .failed:
                         Text("Please try again later...")
@@ -87,7 +87,7 @@ struct EditView: View {
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
