6c07e63b105cf01ac951414c6e474bfa57504f35
Michael Jones <michael.jones90@me.com>
2026-07-05 14:14:18 +0100

    Allowing the user to choose between a standard and hybrid map style.

--- HackingWithSwiftUI-BucketList/Views/ContentView.swift
+++ HackingWithSwiftUI-BucketList/Views/ContentView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var viewModel = ViewModel()
     
+    /// An @AppStorage property to remember the setting issued.
+    @AppStorage("selectedMapStyle") private var selectedMapStyle = "standard"
+    
     /// Defines the initial camera position for a map view, which determines what part of the world is visible when the map first appears.
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
@@ -29,39 +32,49 @@ struct ContentView: View {
     
     var body: some View {
         if viewModel.isUnlocked {
-            /// Creates an interactive map, allowing the user to see existing locations as markers and add new locations by tapping on the map.
-            /// MapReader is a SwiftUI view that provides programmatic access to map-related actions and coordinate conversions.
-            MapReader { proxy in
-                Map(initialPosition: startPosition) {
-                    ForEach(viewModel.locations) { location in
-                        /// Annotation is a view used in MapKit to display custom markers (map annotations).
-                        Annotation(
-                            location.name,
-                            coordinate: location.coordinate
-                        ) {
-                            Image(systemName: "star.circle")
-                                .resizable()
-                                .foregroundStyle(.red)
-                                .frame(width: 44, height: 44)
-                                .background(.white)
-                                .clipShape(.circle)
-                                .onLongPressGesture(minimumDuration: 0.1){
-                                    viewModel.selectedPlace = location
-                                }
+            VStack {
+                /// Creates an interactive map, allowing the user to see existing locations as markers and add new locations by tapping on the map.
+                /// MapReader is a SwiftUI view that provides programmatic access to map-related actions and coordinate conversions.
+                MapReader { proxy in
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            /// Annotation is a view used in MapKit to display custom markers (map annotations).
+                            Annotation(
+                                location.name,
+                                coordinate: location.coordinate
+                            ) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44, height: 44)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .onLongPressGesture(minimumDuration: 0.1){
+                                        viewModel.selectedPlace = location
+                                    }
+                            }
+                        }
+                    }
+                    /// The onTapGesture returns a CGPoint which is converted to CLLocationCoordinate2D from the current view.
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            viewModel.addLocation(at: coordinate)
+                        }
+                    }
+                    .sheet(item: $viewModel.selectedPlace) { location in
+                        EditView(location: location) {
+                            viewModel.update(location: $0)
                         }
                     }
                 }
-                /// The onTapGesture returns a CGPoint which is converted to CLLocationCoordinate2D from the current view.
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        viewModel.addLocation(at: coordinate)
-                    }
+                .mapStyle(selectedMapStyle == "standard" ? .standard : .hybrid)
+                /// Picker View to select the MapStyle to be used.
+                Picker("Map Mode", selection: $selectedMapStyle) {
+                    Text("Standard").tag("standard")
+                    Text("Hybrid").tag("hybrid")
                 }
-                .sheet(item: $viewModel.selectedPlace) { location in
-                    EditView(location: location) {
-                        viewModel.update(location: $0)
-                    }
-                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
             }
         } else {
             Button("Unlock Places", action: viewModel.authenticate)
